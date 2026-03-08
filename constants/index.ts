1e62687634cf347dd12449f5871f0d9c93b918f4
ibrohimmatyoqubov <ibush0925@gmail.com>
2026-03-08 21:08:16 +0500

    add new icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,19 @@
 import { Contact, Home, ListVideo, Rss } from 'lucide-react'
+import {
+	DiCisco,
+	DiCreativecommonsBadge,
+	DiDjango,
+	DiDocker,
+	DiGhost,
+	DiGithubFull,
+	DiLess,
+	DiMailchimp,
+	DiMeteorfull,
+	DiNetmagazine,
+	DiNginx,
+	DiStylus,
+	DiYahoo,
+} from 'react-icons/di'
 
 export const navLinks = [
 	{ route: '', name: 'navLink1', icon: Home },
@@ -12,4 +27,20 @@ export const lngs = [
 	{ route: 'uz', label: "O'zbekcha" },
 	{ route: 'ru', label: 'Русский' },
 	{ route: 'tr', label: 'Türkçe' },
+]
+
+export const companies = [
+	DiCisco,
+	DiCreativecommonsBadge,
+	DiGhost,
+	DiGithubFull,
+	DiMeteorfull,
+	DiLess,
+	DiMailchimp,
+	DiNetmagazine,
+	DiNginx,
+	DiStylus,
+	DiYahoo,
+	DiDjango,
+	DiDocker,
 ]
