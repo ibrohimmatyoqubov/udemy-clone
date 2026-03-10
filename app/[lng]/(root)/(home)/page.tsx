a94ee5be145f64527c016f37ccba9b56ab7d431f
ibrohimmatyoqubov <ibush0925@gmail.com>
2026-03-10 22:55:17 +0500

    add HomePage Components

--- app/[lng]/(root)/(home)/page.tsx
+++ app/[lng]/(root)/(home)/page.tsx
@@ -1,10 +1,18 @@
+import Categories from './_components/categories'
+import FeaturedCourses from './_components/featured-courses'
 import Hero from './_components/hero'
+import Instructor from './_components/instructor'
+import LearningJourney from './_components/learning-journey'
 
 function Page() {
 	return (
-		<>
+		<div className='max-md:px-5'>
 			<Hero />
-		</>
+			<FeaturedCourses />
+			<Categories />
+			<Instructor />
+			<LearningJourney />
+		</div>
 	)
 }
 
