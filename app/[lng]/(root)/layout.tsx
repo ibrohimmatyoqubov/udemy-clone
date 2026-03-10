e85d81d0b8183f3fc0f5eb4bf4aea6f214463d3e
ibrohimmatyoqubov <ibush0925@gmail.com>
2026-03-10 22:51:40 +0500

    add Footer component

--- app/[lng]/(root)/layout.tsx
+++ app/[lng]/(root)/layout.tsx
@@ -1,11 +1,13 @@
 import { ChildProps } from '@/types'
 import Navbar from './_components/navbar'
+import Footer from './_components/footer'
 
 function Layout({ children }: ChildProps) {
 	return (
 		<div>
 			<Navbar />
 			<main>{children}</main>
+			<Footer />
 		</div>
 	)
 }
