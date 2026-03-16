96c4450b1b80de010840d599543281450511cc57
ibrohimmatyoqubov <ibush0925@gmail.com>
2026-03-16 16:47:28 +0500

    add instructor dashboard panel

---
+++ app/[lng]/instructor/my-courses/page.tsx
@@ -0,0 +1,19 @@
+import { courses } from '@/constants'
+import Header from '../_components/header'
+import InstructorCourseCard from '@/components/cards/instructor-course.card'
+
+function Page() {
+	return (
+		<>
+			<Header title='My courses' description='Here are your latest courses' />
+
+			<div className='mt-4 grid grid-cols-3 gap-4'>
+				{courses.map(course => (
+					<InstructorCourseCard key={course.title} {...course} />
+				))}
+			</div>
+		</>
+	)
+}
+
+export default Page
